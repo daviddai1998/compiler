ef2c90e7f75b1ce98c9c3c5a3b2139e70ffced9f
daviddai <wd16@rice.edu>
2019-10-26 01:25:27 -0500

    add rematerialize

--- Allocator.py
+++ Allocator.py
@@ -41,7 +41,7 @@ class Allocator:
 
         if self.rematerial[vr] is not None:
             loadI_ir = self.rematerial[vr]
-            sys.stdout.write("loadI {} => r{} // rematerial\n".format(loadI_ir[R1], pr))
+            sys.stdout.write("loadI {} => r{} \n".format(loadI_ir[R1], pr))
 
         self.VRToPR[vr] = pr
         self.PRToVR[pr] = vr
@@ -69,7 +69,7 @@ class Allocator:
         spill_vr = self.PRToVR[spill_pr]
 
         # if the vr is dirty, just print out for now
-        if self.rematerial[spill_vr] is None:
+        if self.rematerial[spill_vr] is None or not self.clean[spill_vr]:
             # spill_vr type may not be correct
             self.VRToMem[spill_vr] = self.memory_alloc
             # sys.stdout.write("// spill\n")
@@ -84,12 +84,20 @@ class Allocator:
         return spill_pr
 
     def restore(self, vr, pr):
-        if self.rematerial[vr] is None:
-            if self.spilled[vr] and not self.clean[vr]:
-                # sys.stdout.write("// restore\n")
-                sys.stdout.write('loadI %d => r%d\n' % (self.VRToMem[vr], pr))
-                sys.stdout.write('load r%d => r%d\n' % (pr, pr))
-                # sys.stdout.write("// restore finished\n")
+        # if self.spilled[vr] and not self.clean[vr]:
+        #     # sys.stdout.write("// restore\n")
+        #     sys.stdout.write('loadI %d => r%d\n' % (self.VRToMem[vr], pr))
+        #     sys.stdout.write('load r%d => r%d\n' % (pr, pr))
+        #     # sys.stdout.write("// restore finished\n")
+        # elif self.spilled[vr] and self.clean[vr]:
+        #     # sys.stdout.write("// restore\n")
+        #     sys.stdout.write('loadI %d => r%d\n' % (self.VRToVal[vr], pr))
+        #     # sys.stdout.write("// restore finished\n")
+        # self.spilled[vr] = False
+
+        if self.spilled[vr] and self.rematerial[vr] is None:
+            sys.stdout.write('loadI %d => r%d\n' % (self.VRToMem[vr], pr))
+            sys.stdout.write('load r%d => r%d\n' % (pr, pr))
 
     def allocation(self, record):
         ir = record.ir
